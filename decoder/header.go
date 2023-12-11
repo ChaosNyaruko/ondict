e067d598014ac9fd1eee19c1c081806c478fe98f
ChaosNyaruko <cabbageparadise@gmail.com>
2023-12-11 21:55:31 +0800

    decode keyword index, but the ripemd process seems wrong

--- decoder/header.go
+++ decoder/header.go
@@ -7,13 +7,21 @@
 package decoder
 
 import (
+	"bytes"
+	"compress/zlib"
 	"encoding/binary"
 	"encoding/xml"
 	"fmt"
+	"hash/adler32"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"unicode/utf16"
+
+	"github.com/c0mm4nd/go-ripemd"
+	"github.com/cyberdelia/lzo"
 )
 
 type Header struct {
@@ -68,6 +76,150 @@ func Decode(fileName string) error {
 	if err := xml.Unmarshal([]byte(headerXML), &header); err != nil {
 		return err
 	}
+
 	fmt.Printf("header as structured: %+v\n", header)
+
+	// ADLER32 checksum of header, we just read it, not really use for now.
+	var checksum uint32
+	if err := binary.Read(file, binary.LittleEndian, &checksum); err != nil {
+		return err
+	}
+	fmt.Printf("header checksum: %+v\n", checksum)
+
+	encrypt, err := strconv.Atoi(header.Encrypted)
+	if err != nil {
+		return err
+	}
+	if err := decodeKeyWordSection(file, int8(encrypt), header.RegCode); err != nil {
+		return fmt.Errorf("decode keyword section: %v", err)
+	}
+	if err := decodeRecordSection(file); err != nil {
+		return fmt.Errorf("decode record section: %v", err)
+	}
 	return nil
+}
+
+func decodeKeyWordSection(fd io.Reader, encrypted int8, regCode string) error {
+	// num_blocks	8 bytes	Number of items in key_blocks. Big-endian. Possibly encrypted, see below.
+	// num_entries	8 bytes	Total number of keywords. Big-endian. Possibly encrypted, see below.
+	// key_index_decomp_len	8 bytes	Number of bytes in decompressed version of key_index. Big-endian. Possibly encrypted, see below.
+	// key_index_comp_len	8 bytes	Number of bytes in compressed version of key_index (including the comp_type and checksum parts). Big-endian. Possibly encrypted, see below.
+	// key_blocks_len	8 bytes	Total number of bytes taken up by key_blocks. Big-endian. Possibly encrypted, see below.
+	// checksum	4 bytes	ADLER32 checksum of the preceding 40 bytes. Big-endian.
+	// key_index	varying	The keyword index, compressed and possibly encrypted. See below.
+	// key_blocks[0]	varying	A compressed block containing keywords, compressed. See below.
+	// ...	...	...
+	// key_blocks[num_blocks-1]	varying	...
+
+	type keywordSectionHeader struct {
+		NumBlock          uint64
+		NumEntries        uint64
+		KeyIndexDecompLen uint64
+		KeyIndexCompLen   uint64
+		KeyBlockLen       uint64
+	}
+	var header keywordSectionHeader
+	if err := binary.Read(fd, binary.BigEndian, &header); err != nil {
+		return err
+	}
+	if encrypted&1 != 0 {
+		// TODO: the first 40 bytes might be encrypted
+	}
+	var keywordHeaderChecksum [4]byte
+
+	if err := binary.Read(fd, binary.BigEndian, &keywordHeaderChecksum); err != nil {
+		return err
+	}
+	fmt.Printf("raw keyword section header: %#v, checksum: %v\n", header, keywordHeaderChecksum)
+
+	keyIndexEncrypted := (encrypted & 2) != 0
+	// Decrypt keyword Index if encrypted
+	// After this, we will get compressed keyword Index
+	if keyIndexEncrypted {
+		log.Printf("keyword index encrypted, decrypt it")
+		// encrypted by the following C function
+		// #define SWAPNIBBLE(byte) (((byte)>>4) | ((byte)<<4))
+		// void encrypt(unsigned char* buf, size_t buflen, unsigned char* key, size_t keylen) {
+		// 	unsigned char prev=0x36;
+		// 	for(size_t i=0; i < buflen; i++) {
+		// 		buf[i] = SWAPNIBBLE(buf[i] ^ ((unsigned char)i) ^ key[i%keylen] ^ previous);
+		// 		previous = buf[i];
+		// 	}
+		// }
+		keyIndexEncrypted := make([]byte, header.KeyIndexCompLen)
+		if err := binary.Read(fd, binary.BigEndian, keyIndexEncrypted); err != nil {
+			return err
+		}
+		compType := keyIndexEncrypted[:4]
+		compressedChecksum := keyIndexEncrypted[4:8]
+		log.Printf("len(keyIndexEncrypted): %v, %v:%v:%v", len(keyIndexEncrypted), keyIndexEncrypted[:4], keyIndexEncrypted[4:8], keyIndexEncrypted[8:])
+		keyIndexDecrypted := keywordIndexDecrypt(keyIndexEncrypted)
+		log.Printf("len(keyIndexDecrypted): %v, %v:%v:%v", len(keyIndexDecrypted), keyIndexDecrypted[:4], keyIndexDecrypted[4:8], keyIndexDecrypted[8:])
+		decompress(compType, compressedChecksum, keyIndexDecrypted[8:])
+	} else {
+	}
+
+	return nil
+}
+
+func decodeRecordSection(fd io.Reader) error {
+	return nil
+}
+
+func ripemd128(message []byte) []byte {
+	return ripemd.New128().Sum(message)
+}
+
+func keywordIndexDecrypt(data []byte) []byte {
+	key := data[4:8]
+	key = append(key, 0x95, 0x36, 0x00, 0x00)
+	log.Fatalf("key: %v", key)
+	key = ripemd128(key)
+	log.Fatalf("key: %v", key)
+	x := make([]byte, len(data))
+	// The first 8 bytes are **compress type** and **check_sum**
+	copy(x, data)
+	previous := byte(0x36)
+
+	b := x[8:]
+	for i := 0; i < len(b); i++ {
+		t := (b[i]>>4 | b[i]<<4) & 0xff
+		t = t ^ previous ^ (byte(i) & 0xff) ^ key[i%len(key)]
+		previous = b[i]
+		b[i] = t
+	}
+	log.Printf("x[:8]: %v", x[:8])
+	return x
+}
+
+func decompress(compType []byte, checksum []byte, before []byte) []byte {
+	log.Printf("type: %v, checksum: %v", compType, checksum)
+	res := []byte{}
+	decompressed := bytes.NewBuffer(res)
+	in := bytes.NewReader(before)
+	switch compType[0] {
+	// uncompressed, do nothing
+	case 0:
+		io.Copy(decompressed, in)
+	// lzo compressed
+	case 1:
+		if r, err := lzo.NewReader(in); err != nil {
+			log.Fatalf("zlib decompress err: %v", err)
+		} else {
+			io.Copy(decompressed, r)
+			r.Close()
+		}
+	// zlib compressed
+	case 2:
+		if r, err := zlib.NewReader(in); err != nil {
+			log.Fatalf("zlib decompress err: %v", err)
+		} else {
+			io.Copy(decompressed, r)
+			r.Close()
+		}
+	}
+	if adler32.Checksum(res) != binary.BigEndian.Uint32(checksum) {
+		log.Fatalf("checksum not match for decompress!")
+	}
+	return res
 }
--- go.mod
+++ go.mod
@@ -2,16 +2,20 @@ module github.com/ChaosNyaruko/ondict
 
 go 1.20
 
-require golang.org/x/net v0.15.0
+require (
+	github.com/BobuSumisu/aho-corasick v1.0.3
+	github.com/c0mm4nd/go-ripemd v0.0.0-20200326052756-bd1759ad7d10
+	github.com/cyberdelia/lzo v1.0.0
+	github.com/fatih/color v1.15.0
+	github.com/stretchr/testify v1.8.4
+	golang.org/x/net v0.15.0
+)
 
 require (
-	github.com/BobuSumisu/aho-corasick v1.0.3 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/fatih/color v1.15.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
-	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
