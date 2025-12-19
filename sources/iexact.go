e048ebae057255edc2e89d07a1cd194f9b89fadb
ChaosNyaruko <cabbageparadise@gmail.com>
2025-12-19 14:27:55 +0800

    optimize: speed up iexact searcher construction

--- sources/iexact.go
+++ sources/iexact.go
@@ -11,32 +11,34 @@ import (
 
 // IExact is a case in-sensitive search source, no other fuzzy searching algos.
 type IExact struct {
-	dict multimap.MultiMap
+	keys multimap.MultiMap
+	dict Dict
 }
 
 var _ Searcher = &IExact{}
 
 func NewIExact(dict Dict) Searcher {
-	d := slicemultimap.New()
+	keys := slicemultimap.New()
 	total := len(dict.Keys())
-	// TODO: it takes a lot of time on low-end machines.
 	bar := progressbar.Default(int64(total), fmt.Sprintf("constructing iexact searcher"))
+
 	for _, k := range dict.Keys() {
 		lk := strings.ToLower(k)
-		d.Put(lk, dict.Get(k))
+		keys.Put(lk, k)
 		bar.Add(1)
 	}
-	return &IExact{dict: d}
+	return &IExact{dict: dict, keys: keys}
 }
 
 func (e *IExact) GetRawOutputs(input string) []RawOutput {
 	res := make([]RawOutput, 0, 1)
-	ro, _ := e.dict.Get(strings.ToLower(input))
+	ro, _ := e.keys.Get(strings.ToLower(input))
 
 	for _, r := range ro {
+		def := e.dict.Get(r.(string))
 		res = append(res, output{
 			rawWord: input,
-			def:     r.(string),
+			def:     def,
 		})
 	}
 	return res
