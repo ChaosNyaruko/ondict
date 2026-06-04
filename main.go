20ba375b1276265898a87a73ed1ad66b3a11373d
chaosnyaruko <cabbageparadise@gmail.com>
2026-06-04 18:51:49 +0800

    test sync func

--- main.go
+++ main.go
@@ -78,14 +78,15 @@ var file *os.File
 
 func init() {
 	var err error
-	file, err = os.OpenFile(filepath.Join(util.TmpDir(), "ondict.log"), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	filename := filepath.Join(util.TmpDir(), "ondict.log")
+	fmt.Fprintf(os.Stderr, "This log will be written to a file. %q\n", filename)
+	file, err = os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err == nil {
 		log.SetOutput(file)
 	} else {
 		log.Info("Failed to log to file, using default stderr")
 	}
 
-	log.Info("This log will be written to a file.")
 	log.SetLevel(log.TraceLevel)
 }
 
--- server.go
+++ server.go
@@ -107,6 +107,7 @@ func mountSyncServer(r *gin.Engine) error {
 		return fmt.Errorf("ONDICT_SYNC_USER / ONDICT_SYNC_PASSWORD env vars must be set")
 	}
 	dir := *syncDataDir
+	fmt.Fprintf(os.Stderr, "sync data dir=%q\n", dir)
 	if dir == "" {
 		dir = filepath.Join(util.ConfigPath(), "sync")
 	}
